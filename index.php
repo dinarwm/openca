c086de81d165e7bb569c17c3ad3c6c263e8bfca3
Risyanggi Azmi Faizin <switgarlik@gmail.com>
2015-05-07 02:34:39 +0700

    ngerapihin -_-

--- index.php
+++ index.php
@@ -1,79 +1,79 @@
+<!doctype html>
 <html>
 <head>
-	<title>
-		Membuat Sertifikat
-	</title>
-	<link href="css/bootstrap.css" rel="stylesheet">	
+    <title>
+        Membuat Sertifikat
+    </title>
+    <link href="css/bootstrap.css" rel="stylesheet">    
 </head>
 <body>
-	<div class="container">
-		<label><h1>Certificate Form</h1></label>
-		<form action="createCertificate.php" method="POST">
-			<label>
-				<h5>Name</h5>
-			</label>
-			<input type="text" class="input-block-level" name="commonName">
-			<label><h5>Country</h5></label>
-			<table class="table">
-				<thead>
-					<tr>
-						<td>
-							<label class="radio">
-								<input type="radio" name="country" id="optionsRadios1" value="ID" checked=""> Indonesia 
-							</label>
-						</td>
-						<td>
-							<label class="radio">
-								<input type="radio" name="country" id="optionsRadios1" value="US"> USA
-							</label> 
-						</td>
-						<td>
-							<label class="radio ">
-								<input type="radio" name="country" id="optionsRadios1" value="CA"> Canada 
-							</label> 
-						</td>
-						<td>
-							<label class="radio">		
-								<input type="radio" name="country" id="optionsRadios1" value="FR"> France
-							</label>
-						</td>
-					<td>
-						<label class="radio">		
-							<input type="radio" name="country" id="optionsRadios1" value="JP"> Japan
-						</label>
-					</td>
-					<td>
-						<label class="radio">		
-							<input type="radio" name="country" id="optionsRadios1" value="QA"> Qatar
-						</label>
-					</td>
-				</tr>
-				</thead>
-			</table>
-
-			<label>
-				<h5>State Or Province Name</h5>
-			</label>
-			<input type="text" class="input-block-level" name="province">
-			<label>
-				<h5>Locality Name</h5>
-			</label>
-			<input type="text" class="input-block-level" name="locality">
-			<label>
-				<h5>Organization Name</h5>
-			</label>
-			<input type="text" class="input-block-level" name="organization">
-			<label>
-				<h5>Organizational Unit Name</h5>
-			</label>
-			<input type="text" class="input-block-level" name="organizationUnit">
-			<label>
-				<h5>Email Address</h5>
-			</label>
-			<input type="email" class="input-block-level" name="email">
-			<br>
-			<br>
-			<input type="submit" class="btn btn-large btn-primary btn-block" value="submit">
-		</form>
-	</div>
+    <div class="container">
+    <h1 class="text-center">Certificate Form</h1>
+        <div class="row">
+            <div class="col-md-offset-2 col-md-8">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <form action="createCertificate.php" method="POST" class="form-horizontal">
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Nama</label>
+                                <div class="col-md-6">
+                                    <input class="form-control" placeholder="Name" name="commonName" type="text">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Country</label>
+                                <div class="col-md-6">
+                                    <select class="form-control" name="country">
+                                        <option value="ID">Indonesia</option>
+                                        <option value="US">USA</option>
+                                        <option value="CA">Canada</option>
+                                        <option value="FR">France</option>
+                                        <option value="JP">Japan</option>
+                                        <option value="QA">Qatar</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">State or Province</label>
+                                <div class="col-md-6">
+                                    <input class="form-control" placeholder="Name" name="province" type="text">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Locality</label>
+                                <div class="col-md-6">
+                                    <input class="form-control" placeholder="Name" name="locality" type="text">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Organization Name</label>
+                                <div class="col-md-6">
+                                    <input class="form-control" placeholder="Name" name="organization" type="text">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Organizational Unit Name</label>
+                                <div class="col-md-6">
+                                    <input class="form-control" placeholder="Name" name="organizationUnit" type="text">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Email</label>
+                                <div class="col-md-6">
+                                    <input class="form-control" name="email" type="email">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Submit
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
